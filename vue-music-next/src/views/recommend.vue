005daa16eb11e7a53c3adc68b6d354a94f29d24f
huangzhihui <leononon412@gmail.com>
2022-09-02 17:59:37 +0800

    add v-loading

--- vue-music-next/src/views/recommend.vue
+++ vue-music-next/src/views/recommend.vue
@@ -1,32 +1,68 @@
 <template>
-  <div class="recommend">
-    <div class="slider-wrapper">
-      <div class="slider-content">
-        <slider v-if="sliders.length" :sliders="sliders"></slider>
+  <div class="recommend" v-loading:[loadingText]="loading">
+    <scroll class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+          <div class="slider-content">
+            <slider v-if="sliders.length" :sliders="sliders"></slider>
+          </div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title" v-show="!loading">热门歌单推荐</h1>
+          <ul>
+            <li
+              v-for="item in albums"
+              class="item"
+              :key="item.id"
+            >
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.pic" />
+              </div>
+              <div class="text">
+                <h2 class="name">
+                  {{ item.username }}
+                </h2>
+                <p class="title">
+                  {{ item.title }}
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
-import { getRecommend } from "@/service/recommend";
-import Slider from "@/components/base/slider/slider";
+import { getRecommend } from '@/service/recommend'
+import Slider from '@/components/base/slider/slider'
+import Scroll from '@/components/base/scroll/scroll'
 
 export default {
-  namme: "recommend",
+  name: 'recommend',
   components: {
     Slider,
+    Scroll
   },
-  data(){
+  data() {
     return {
-        sliders:[]
+      sliders: [],
+      albums: [],
+      loadingText: '正在载入...'
     }
   },
   async created() {
-    const result = await getRecommend();
+    const result = await getRecommend()
     this.sliders = result.sliders
+    this.albums = result.albums
   },
-};
+  computed: {
+    loading() {
+      return !this.sliders.length && !this.albums.length
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
